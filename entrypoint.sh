cc264fc5125e969bd57e5f38e239c045adca2e74
Scott Brenner <scott@scottbrenner.me>
2021-03-13 20:32:15 -0800

    fix(entrypoint.sh) - generate from specific tag or range

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ if [ "$1" ] && [ "$1" != "package.json" ]; then
   cp "$1" package.json
 fi
 
-tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
-changelog=$(generate-changelog -t "$tag" --file -)
+previous_tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
+new_tag=$(git tag --sort version:refname | tail -n 1)
+changelog=$(generate-changelog -t "$previous_tag..$new_tag" --file -)
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
