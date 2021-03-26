c6739e727bd7e03b9b9edcf3bb4307ac0fe9e8ce
Fabricio Sander <fabricio@codeshop.ai>
2021-03-26 19:24:32 -0300

    fix: fixes variable value when to-tag is found

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" ] && [ "$1" != "package.json" ]; then
 fi
 
 [ -z "$FROM_TAG" ] && { echo "No from-tag passed. Fallbacking to git previous tag."; previous_tag=$(git tag --sort version:refname | tail -n 2 | head -n 1); } || { echo "From-tag detected. Using it's value."; previous_tag=$FROM_TAG; }
-[ -z "$TO_TAG"   ] && { echo "No to-tag passed. Fallbacking to git previous tag."  ;      new_tag=$(git tag --sort version:refname | tail -n 1);             } || { echo "To-tag detected. Using it's value.";        new_tag=$FROM_TAG; }
+[ -z "$TO_TAG"   ] && { echo "No to-tag passed. Fallbacking to git previous tag."  ;      new_tag=$(git tag --sort version:refname | tail -n 1);             } || { echo "To-tag detected. Using it's value.";          new_tag=$TO_TAG; }
 changelog=$(generate-changelog -t "$previous_tag..$new_tag" --file -)
 
 changelog="${changelog//'%'/'%25'}"
