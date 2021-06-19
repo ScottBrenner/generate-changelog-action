a5e2ac337d0ab6e014e015549b8991f3fefbd589
Scott Brenner <scott@scottbrenner.me>
2021-06-19 15:52:13 -0700

    shellcheck disable=SC2086

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,7 +61,7 @@ else
   unknown_commits="--allow-unknown "
 fi
 
-changelog=$(generate-changelog "$changelog_type" -t "$previous_tag..$new_tag" ${exclude_types} "$unknown_commits" --file -)
+changelog=$(generate-changelog "$changelog_type" -t "$previous_tag..$new_tag" ${exclude_types} "$unknown_commits" --file -) # shellcheck disable=SC2086
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
